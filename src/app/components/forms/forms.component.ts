32b86e5ec107a4786dcc51c9a933e36aa97850ab
Tarik Raposo <tarikraposo@gmail.com>
2024-09-05 18:44:52 -0300

    fix: stop propagation

--- src/app/components/forms/forms.component.ts
+++ src/app/components/forms/forms.component.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, Directive, HostListener, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  FormResetEvent,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-forms',
   standalone: true,
   imports: [],
   templateUrl: './forms.component.html',
-  styleUrl: './forms.component.scss'
+  styleUrl: './forms.component.scss',
 })
-export class FormsComponent {
+export class FormsComponent implements OnInit {
+  result = [];
+  formGroup!: FormGroup;
 
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.formGroup = this.formBuilder.group({
+      hour: '',
+      showClosed: false,
+    });
+  }
+
+  onSubmit(e:Event) {
+    e.preventDefault();
+    e.stopPropagation();
+    console.log(this.formGroup.value);
+  }
+
+  onClean() {
+    this.formGroup.reset();
+  }
 }
